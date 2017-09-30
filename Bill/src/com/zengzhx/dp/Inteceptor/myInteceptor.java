5e1b2fa0090263df7874c31743bf8401287dcd9b
zengzhixiong <zengzhixiong@zhizhangyi.com>
2017-09-30 18:30:29 +0800

    初始化

---
+++ Bill/src/com/zengzhx/dp/Inteceptor/myInteceptor.java
@@ -0,0 +1,13 @@
+package com.zengzhx.dp.Inteceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+
+public class myInteceptor extends HandlerInterceptorAdapter {
+
+	
+	public void doit(){
+		
+	}
+
+}
---
+++ Bill/src/com/zengzhx/dp/model/User.java
@@ -0,0 +1,22 @@
+package com.zengzhx.dp.model;
+
+public class User {
+	
+	
+	public User(){
+		System.out.println("º”‘ÿAA£°£°£°£°£°");
+	}
+	
+	public User(int AA){
+		System.out.println("º”‘ÿAAAAAAAA£°£°£°£°£°");
+	}
+	private int AA=0;
+
+	public int getAA() {
+		return AA;
+	}
+
+	public void setAA(int aA) {
+		AA = aA;
+	}
+}
